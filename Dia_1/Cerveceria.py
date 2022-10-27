6b5f680833028dfd7616c5940e74383209b2d644
AdxDaz <105800178+AdxDaz@users.noreply.github.com>
2022-10-27 14:08:04 -0300

    habia que especificar cuando

--- Dia_1/Cerveceria.py
+++ Dia_1/Cerveceria.py
@@ -1,4 +1,4 @@
-print("El nombre de tu cerveza\nes '" + input("Que ciudad te gustaria visitar?: ") + " " + input("Cual es tu color favorito?: ") + "'\nFelicitaciones!")
+print("El nombre de tu cerveza\nes '" + input("Que ciudad te gustaria visitar de nuevo?: ") + " " + input("Cual es tu color favorito?: ") + "'\nFelicitaciones!")
 
 
 
