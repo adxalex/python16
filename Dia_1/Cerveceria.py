36f67db0dc8b456942ae4a0bb6d38b7386b46a13
adx/alex <adx.alex@gmail.com>
2023-01-10 08:21:16 -0300

    ok

--- Dia_1/Cerveceria.py
+++ Dia_1/Cerveceria.py
@@ -1,6 +1,11 @@
-print("El nombre de tu cerveza\nes '" + input("Que ciudad te gustaria visitar de nuevo?: ") + " " + input("Cual es tu color favorito?: ") + "'\nFelicitaciones!")
+#print("El nombre de tu cerveza\nes '" + input("Que ciudad te gustaria visitar de nuevo?: ") + " " + input("Cual es tu color favorito?: ") + "'\nFelicitaciones!")
 
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
 
+p1 = Person("John", 36)
 
-
-
+print(p1.name)
+print(p1.age)
